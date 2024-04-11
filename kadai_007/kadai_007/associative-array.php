8755199c7bff24387c4bcf25ca5d74d2c3d856bd
One-Master-Yellow <149803197+One-Master-Yellow@users.noreply.github.com>
2024-04-11 21:00:38 +0900

    Add files via upload

---
+++ kadai_007/kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <tittle>PHP基礎編</tittle>
+</head>
+
+<body>
+    <p>
+        <?php
+        $material_data = ['name' => 'onion', 'price' =>200, 'weight' => '160'];
+
+        print_r($material_data);
+        
+        ?>
+    </p>
+
+</body>
+
+</html>
