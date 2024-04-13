6ba3ae6dd465f00ffade14b5c8a1a7d1d7800f2a
One-Master-Yellow <149803197+One-Master-Yellow@users.noreply.github.com>
2024-04-13 23:59:49 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $score1 = 80;
+        $score2 = 60;
+        $score3 = 55;
+        $score4 = 40;
+        $score5 = 100;
+        $score6 = 25;
+        $score7 = 80;
+        $score8 = 95;
+        $score9 = 30;
+        $score10 = 60;
+
+        echo score(1+2+3+4+5+6+7+8+9+10)/10;
+
+        echo'<br>';
+        ?>
+    </p>
+</body>
+
+</html>
